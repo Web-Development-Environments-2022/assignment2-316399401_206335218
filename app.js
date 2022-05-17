a5fd4a27d8ef83031f0c56f3a1972588196622ec
NoaBiren <noabiren@post.bgu.ac.il>
2022-05-17 11:20:43 +0300

    ghost pictures, pacman pictures, timer, settings view

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var context;
-var shape = new Object();
+var shape1 = new Object();
 var board;
 var score;
 var pac_color;
@@ -14,12 +14,13 @@ var p5num;
 var p15num;
 var p25num;
 var sumCount=0;
+var monsCount=0;
+var pacmanDirection="RIGHT";
 
 
 $(document).ready(function() {
 	context = canvas.getContext("2d");
-
-	Start();
+	// Start();
 });
 
 function foodDev(){
@@ -53,12 +54,12 @@ function Start() {
 	pac_color = "yellow";
 	var cnt = 16*16;
 	var food_remain = foodNum;
+
 	foodDev();
 	var pacman_remain = 1;
 	start_time = new Date();
 	for (var i = 0; i < 17; i++) {
 		board[i] = new Array();
-		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
 		for (var j = 0; j < 17; j++) {
 			if (
 				(j == 0) ||
@@ -151,7 +152,23 @@ function Start() {
 
 			) {
 				board[i][j] = 4;
-			} else {
+			}else if(j==1 && i==1 && monsCount<monstersNum){
+				board[i][j]=6 //green
+				monsCount++;
+			} 	else if (j==15 && i==15 && monsCount<monstersNum){
+					board[i][j]=7 //blue
+					monsCount++;
+				}
+				else if (j==15 && i==1 && monsCount<monstersNum){
+					board[i][j]=8 //"orange"
+					monsCount++;
+				}
+				else if (j==1 && i==15 && monsCount<monstersNum){
+					board[i][j]=9 //"red"
+					monsCount++;
+				}
+			else {
+
 				var randomNum = Math.random();
 				if (randomNum <= (1.0 * food_remain) / cnt) {
 					sumCount++;
@@ -166,8 +183,8 @@ function Start() {
 						board[i][j] = 25;
 					}
 				} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
-					shape.i = i;
-					shape.j = j;
+					shape1.i = i;
+					shape1.j = j;
 					pacman_remain--;
 					board[i][j] = 2;
 				} else {
@@ -207,18 +224,22 @@ function Start() {
 		},
 		false
 	);
-	interval = setInterval(UpdatePosition, 250);
+	if (shape1 == undefined){
+		shape1 = new Object();
+	}
+	interval = setInterval(UpdatePosition, 50);
+	console.log(board);
 }
 
 
 
 
 function findRandomEmptyCell(board) {
-	var i = Math.floor(Math.random() * 15 + 1);
-	var j = Math.floor(Math.random() * 15 + 1);
+	var i = Math.floor(Math.random() * 16 + 1);
+	var j = Math.floor(Math.random() * 16 + 1);
 	while (board[i][j] != 0) {
-		i = Math.floor(Math.random() * 15 + 1);
-		j = Math.floor(Math.random() * 15 + 1);
+		i = Math.floor(Math.random() * 16 + 1);
+		j = Math.floor(Math.random() * 16 + 1);
 	}
 	return [i, j];
 }
@@ -242,45 +263,77 @@ function Draw() {
 	canvas.width = canvas.width; //clean board
 	lblScore.value = score;
 	lblTime.value = time_elapsed;
-	for (var i = 0; i < 18; i++) {
-		for (var j = 0; j < 18; j++) {
+	lblCurUser.value = current_username;
+	for (var i = 0; i < 17; i++) {
+		for (var j = 0; j < 17; j++) {
 			var center = new Object();
-			center.x = i * 60 + 30;
-			center.y = j * 60 + 30;
+			center.x = i * 40 + 20;
+			center.y = j * 40 + 20;
 			if (board[i][j] == 2) {
-				context.beginPath();
-				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
-				context.lineTo(center.x, center.y);
-				context.fillStyle = pac_color; //color
-				context.fill();
-				context.beginPath();
-				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
-				context.fill();
+				shape1.i = i;
+				shape1.j = j;
+				var pacmanImg = new Image();
+				if (pacmanDirection == "RIGHT"){
+					pacmanImg.src = "Pac-Man-right.png"
+				}
+				if (pacmanDirection == "UP"){
+					pacmanImg.src = "Pac-Man-up.png"
+				}
+				if (pacmanDirection == "DOWN"){
+					pacmanImg.src = "Pac-Man-down.png"
+				}
+				if (pacmanDirection == "LEFT"){
+					pacmanImg.src = "Pac-Man-left.png"
+				}
+				
+    			context.drawImage(pacmanImg, center.x - 20, center.y - 20, 40, 40);
 			} else if (board[i][j] == 5) {
 				sumCount++;
 				context.beginPath();
-				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
+				context.arc(center.x, center.y, 12, 0, 2 * Math.PI); // circle
 				context.fillStyle = p5color;
 				context.fill();
 			} else if (board[i][j] == 15) {
 				sumCount++;
 				context.beginPath();
-				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
+				context.arc(center.x, center.y, 9, 0, 2 * Math.PI); // circle
 				context.fillStyle = p15color;
 				context.fill();
 			}  else if (board[i][j] == 25) {
 			sumCount++;
 			context.beginPath();
-			context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
+			context.arc(center.x, center.y, 7, 0, 2 * Math.PI); // circle
 			context.fillStyle = p25color;
 			context.fill();
-		} 
-		 else if (board[i][j] == 4) {
-				context.beginPath();
-				context.rect(center.x - 30, center.y - 30, 60, 60);
-				context.fillStyle = "grey"; //color
-				context.fill();
+			} 
+			else if (board[i][j] == 4) {
+					context.beginPath();
+					context.rect(center.x - 20, center.y - 20, 40, 40);
+					context.fillStyle = "grey"; //color
+					context.fill();
+			}
+			else if (board[i][j] == 9){ //red
+				var redImg = new Image();
+				redImg.src = "red-ghost.png";
+				context.drawImage(redImg,center.x-20,center.y-20,40,40);
+			}
+			else if (board[i][j] == 6){ //green
+				var greenImg = new Image();
+				greenImg.src = "green-ghost.png"
+    			context.drawImage(greenImg, center.x - 20, center.y - 20, 40, 40);
+
+				
+			}
+			else if (board[i][j] == 7){ //blue
+				var blueImg = new Image();
+				blueImg.src = "blue-ghost.png"
+    			context.drawImage(blueImg, center.x - 20, center.y - 20, 40, 40);
+
+			}
+			else if (board[i][j] == 8){ //orange
+				var orangeImg = new Image();
+				orangeImg.src = "orange-ghost.png"
+    			context.drawImage(orangeImg, center.x - 20, center.y - 20, 40, 40);
 			}
 		}
 	}
@@ -291,47 +344,55 @@ function Draw() {
 
 
 function UpdatePosition() {
-	board[shape.i][shape.j] = 0;
+	if (shape1.i==undefined || shape1.j==undefined){
+		return;
+	}
+	board[shape1.i][shape1.j] = 0;
 	var x = GetKeyPressed();
-	if (x == 1) {
-		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
-			shape.j--;
+	if (x == 1) { //up
+		if (shape1.j > 0 && board[shape1.i][shape1.j - 1] != 4) {
+			shape1.j--;
+			pacmanDirection = "UP";
 		}
 	}
-	if (x == 2) {
-		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
-			shape.j++;
+	if (x == 2) { //down
+		if (shape1.j < 16 && board[shape1.i][shape1.j + 1] != 4) {
+			shape1.j++;
+			pacmanDirection = "DOWN";
 		}
 	}
-	if (x == 3) {
-		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
-			shape.i--;
+	if (x == 3) { //left
+		if (shape1.i > 0 && board[shape1.i - 1][shape1.j] != 4) {
+			shape1.i--;
+			pacmanDirection = "LEFT";
 		}
 	}
-	if (x == 4) {
-		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
-			shape.i++;
+	if (x == 4) { //right
+		if (shape1.i < 16 && board[shape1.i + 1][shape1.j] != 4) {
+			shape1.i++;
+			pacmanDirection = "RIGHT";
 		}
 	}
-	if (board[shape.i][shape.j] == 5) {
+	if (board[shape1.i][shape1.j] == 5) {
 		score+=5;
 	}
-	if (board[shape.i][shape.j] == 15) {
+	if (board[shape1.i][shape1.j] == 15) {
 		score+=15;
 	}
-	if (board[shape.i][shape.j] == 25) {
+	if (board[shape1.i][shape1.j] == 25) {
 		score+=25;
 	}
-	board[shape.i][shape.j] = 2;
+	board[shape1.i][shape1.j] = 2;
 	var currentTime = new Date();
-	time_elapsed = (currentTime - start_time) / 1000;
-	if (score >= 20 && time_elapsed <= 10) {
-		pac_color = "green";
-	}
-	if (score == 50) { 
+	time_elapsed = time - (currentTime - start_time) / 1000;
+	time_elapsed = Math.floor(time_elapsed);
+
+
+	if (time_elapsed == 0) { 
 		window.clearInterval(interval);
 		window.alert("Game completed");
 	} else {
 		Draw();
 	}
 }
+
