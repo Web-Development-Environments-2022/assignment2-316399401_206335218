8175e5bc5c875b9d0a323518ac1b27ed434ac6d8
NoaBiren <noabiren@post.bgu.ac.il>
2022-05-17 11:21:28 +0300

    timer, pacman picture, ghosts pictures, settings view

--- settings.js
+++ settings.js
@@ -21,6 +21,67 @@ var point25="#d8a8ff";
     
 // }
 
+function chooseKey(keyname){
+    $(document).keydown(function(event){
+        if (keyname == "keyUp"){
+            document.getElementById("keyUp").value = "";
+            var code = event.keyCode;
+            if (code==chosenKeys["keyDown"] || code==chosenKeys["keyRight"] || code==chosenKeys["keyLeft"]){
+                alert("You already chose this key, please choose another one :)")
+                $(document).unbind();
+                return;
+            }
+            var char = keyPressed(code);
+            document.getElementById("keyUp").value = char;
+            document.getElementById("upVal").innerHTML = char;
+            chosenKeys["keyUp"]= code;
+        }
+        else if (keyname == "keyDown"){
+            document.getElementById("keyDown").value = "";
+            var code = event.keyCode;
+            if (code==chosenKeys["keyUp"] || code==chosenKeys["keyRight"] || code==chosenKeys["keyLeft"]){
+                alert("You already chose this key, please choose another one :)")
+                $(document).unbind();
+                return;
+            }
+            var char = keyPressed(code);
+            document.getElementById("keyDown").value = char;
+            document.getElementById("downVal").innerHTML = char;
+            chosenKeys["keyDown"]= code;
+
+        }
+        else if (keyname == "keyLeft"){
+            document.getElementById("keyLeft").value = "";
+            var code = event.keyCode;
+            if (code==chosenKeys["keyDown"] || code==chosenKeys["keyRight"] || code==chosenKeys["keyUp"]){
+                alert("You already chose this key, please choose another one :)")
+                $(document).unbind();
+                return;
+            }
+            var char = keyPressed(code);
+            document.getElementById("keyLeft").value = char;
+            document.getElementById("leftVal").innerHTML = char;
+            chosenKeys["keyLeft"]= code;
+
+        }
+        else if (keyname == "keyRight"){
+            document.getElementById("keyRight").value = "";
+            var code = event.keyCode;
+            if (code==chosenKeys["keyDown"] || code==chosenKeys["keyUp"] || code==chosenKeys["keyLeft"]){
+                alert("You already chose this key, please choose another one :)")
+                $(document).unbind();
+                return;
+            }
+            var char = keyPressed(code);
+            document.getElementById("keyRight").value = char;
+            document.getElementById("rightVal").innerHTML = char;
+            chosenKeys["keyRight"]= code;
+
+        }
+        $(document).unbind();
+    })
+}
+
 
 function keyPressed(keycode){
     if (keycode==38){
@@ -42,7 +103,7 @@ function changedValue(range, label, theVar,settingView){
     var val = document.getElementById(range).value
     document.getElementById(label).innerHTML = val;
     window[theVar] = val;
-    document.getElementById(settingView).value = val;
+    document.getElementById(settingView).innerHTML = val;
 
 }
 
